12f8f5bf8f8e75b64d57fad003a5044613ac4018
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-03-02 15:44:43 +0530

    initial

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function App() {
+  return (
+    <div>
+      <h1>EXPENCE MANAGER</h1>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+const Nav = () => {
+  return (
+    <div>
+      <p>
+        <button>Home</button>
+        <button>About</button>
+        <button>Contact</button>
+      </p>
+    </div>
+  );
+};
+
+export default Nav;
