137cddbe1b1a16d4b8fc85bbf2db3bca537fe422
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-03-03 15:08:11 +0530

    initial

--- src/components/Operation.js
+++ src/components/Operation.js
@@ -2,6 +2,13 @@ const Operation = () => {
   return (
     <div>
       <h1 className="h1">Personal Expense Manager</h1>
+      <operation className="operation">
+        <button className="add-btn">Add Expence</button>
+        <span className="filter">
+          <input className="search" type="text" placeholder="Search"></input>
+          <button className="filter-btn">Filter</button>
+        </span>
+      </operation>
     </div>
   );
 };
